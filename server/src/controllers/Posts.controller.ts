f0091844bb9fecca4f2e0ef4abe1eb5f5295bb9d
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-06-24 19:29:26 +0530

    createPost Completed

--- server/src/controllers/Posts.controller.ts
+++ server/src/controllers/Posts.controller.ts
@@ -13,9 +13,24 @@ export const getPosts = async (req: Request, res: Response) => {
 };
 
 export const createPost = async (req: Request, res: Response) => {
-  res.status(201).json({
-    message: "Post created successfully",
-  });
+  try {
+
+    const { title, content } = req.body;
+
+    const newPost = new Post ({
+      content,
+      title
+    })
+
+    await newPost.save()
+
+    res.status(201).json({
+      message: "Post created successfully",
+    });
+  } catch (error) {
+    console.log("error is:", error);
+    res.status(500).json({message: "Internal Server Error"});
+  }
 };
 
 export const updatePost = async (req: Request, res: Response) => {
