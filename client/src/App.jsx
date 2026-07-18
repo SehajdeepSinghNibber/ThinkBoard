d27b868d8b0317ccb0e4af5a773b6b174dff8f6d
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-07-18 14:47:41 +0530

    default theme set to abyss using daisyui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import toast from 'react-hot-toast'
 const App = () => {
   return (
     <div>
-      <button onClick={()=>toast.success("Congrats")} className='text-red-500'>Click Me</button>
+      <button onClick = {()=>toast.success("Done")} className='btn btn-primary btn-outline'>Click Me</button>
       <Routes>
         <Route  path = "/" element={<HomePage />}/>
         <Route  path = "/create" element={<CreatePage />}/>
