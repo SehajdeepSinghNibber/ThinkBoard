ecd189cbddeba00db15ccda3a20db6e6a60e9f7c
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-07-18 19:21:50 +0530

    rate limited box added, cors added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
 import Navbar from "../component/Navbar"
+import RateLimitedUi from "../component/RateLimitedUi";
 
 const HomePage = () => {
+
+  const [isRateLimited, setRateLimited] = useState(false);
+  const [notes,setNotes] = useState([])
+  const [loading,setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/api/v1/posts");
+        console.log(res.data);
+      } catch (error) {
+        console.error(error);
+        console.log("Error fetching posts");
+      }
+    };
+
+      fetchNotes();
+    }, []);
+
   return (
     <div className="min-h-screen">
         <Navbar />
+
+        {isRateLimited && <RateLimitedUi />}
     </div>
   )
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import router from './routes/Posts.route';
 import rateLimiter from './middleware/rateLimiter';
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
 
 app.use(rateLimiter);
+
+app.use(cors());
 
 app.use("/api/v1/posts",router);
 
