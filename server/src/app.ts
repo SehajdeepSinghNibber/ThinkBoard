ea35587a11027882e8688e4d4a32047568a6a7ba
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-07-19 10:31:25 +0530

    helmet and morgan added

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import router from './routes/Posts.route';
 import rateLimiter from './middleware/rateLimiter';
 import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan";
+
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(helmet());
 
 app.use(rateLimiter);
 
